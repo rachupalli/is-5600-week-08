8f8053cdfc0645214ff4b60463d2021c9fb2a447
rachupalli <141514256+rachupalli@users.noreply.github.com>
2025-03-09 18:54:14 +0000

    created orders.test.js

--- tests/products.test.js
+++ tests/products.test.js
@@ -1,20 +1,28 @@
 const { it } = require('node:test');
-const { list } = require('../products');
 const { mockDb, mockProducts } = require('./db.mock');
+const { list } = require('../products');
 const productTestHelper = require('./test-utils/productTestHelper');
 
+jest.mock('../db', () => mockDb);
 describe('Product Module', () => {
 
-    beforeAll(async () => {
-        await productTestHelper.setupTestData();
-      });
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    // beforeAll(async () => {
+    //     await productTestHelper.setupTestData();
+    //   });
     
-      afterAll(async () => {
-        await productTestHelper.cleanupTestData();
-      });
+    //   afterAll(async () => {
+    //     await productTestHelper.cleanupTestData();
+    //   });
       
     test('should list all products', async () => {
       const products = await list();
-      expect(products.length).toBeGreaterThan(0);
+
+      expect(products.length).toBe(2);
+      expect(products[0].description).toBe('Product 1');
+      expect(products[1].description).toBe('Product 2');
     });
   });
