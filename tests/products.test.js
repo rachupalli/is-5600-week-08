98592028a61a6275ee5bbc587b9a970cb3830194
rachupalli <141514256+rachupalli@users.noreply.github.com>
2025-03-09 18:42:31 +0000

    created db.mock.js

--- tests/products.test.js
+++ tests/products.test.js
@@ -1,7 +1,18 @@
 const { it } = require('node:test');
 const { list } = require('../products');
+const { mockDb, mockProducts } = require('./db.mock');
+const productTestHelper = require('./test-utils/productTestHelper');
 
 describe('Product Module', () => {
+
+    beforeAll(async () => {
+        await productTestHelper.setupTestData();
+      });
+    
+      afterAll(async () => {
+        await productTestHelper.cleanupTestData();
+      });
+      
     test('should list all products', async () => {
       const products = await list();
       expect(products.length).toBeGreaterThan(0);
